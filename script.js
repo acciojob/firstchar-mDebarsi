85f7afe7d402ac972c2488e8ea9760cc449d7034
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-13 20:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstChar(text) {
   // your code here
+	// Trim leading and trailing spaces
+  const trimmedStr = text.trim();
+
+  // Check if the trimmed string is not empty
+  if (trimmedStr !== '') {
+    // Return the first character of the trimmed string
+    return trimmedStr.charAt(0);
+  } else {
+    // Return an empty string if the trimmed string is empty
+    return '';
+  }
 }
 
 // Do not change the code below
